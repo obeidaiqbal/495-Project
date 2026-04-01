601236bfcfc199f873613edbef0e52fa46b483af
Obeida Iqbal <obeidaiqbal41@gmail.com>
2026-04-01 10:29:59 -0400

    Added button and script that once pressed updates container with new descriptions, we can update the descriptions array for each specific scenario

--- js/main.js
+++ js/main.js
@@ -1,9 +1,13 @@
 
+
 // Function Is Endpoint for Adding Newest Description
+let descriptionCount = 0
 function updateDescription(newDescription) {
     const description = document.getElementById("description");
     const sound = document.getElementById("alert");
     const prev = document.createElement("p");
+    prev.id = "desc-" + descriptionCount;
+    descriptionCount++;
 
     prev.textContent = newDescription;
     description.appendChild(prev);
