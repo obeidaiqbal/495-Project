c2d4a99fda31a846855e3659c699916a7be524fd
Miles Gilberti <milgil@umich.edu>
2026-04-01 20:43:13 -0400

    text to speech logic

--- js/main.js
+++ js/main.js
@@ -2,16 +2,36 @@
 
 // Function Is Endpoint for Adding Newest Description
 let descriptionCount = 0
+let currentDescription = 0
+
 function updateDescription(newDescription) {
     const description = document.getElementById("description");
     const sound = document.getElementById("alert");
     const prev = document.createElement("p");
     prev.id = "desc-" + descriptionCount;
     descriptionCount++;
+    currentDescription = descriptionCount;
 
     prev.textContent = newDescription;
     description.appendChild(prev);
     description.scrollTop = description.scrollHeight;
     sound.currentTime = 0;
     sound.play();
+}
+//add buttons for playDescription, rewind, forward in the scenario thingy
+
+function playDescription() {
+    const description = document.getElementById("desc-"+currentDescription); 
+    const sound = new SpeechSynthesisUtterance(description);
+    window.speechSynthesis.speak(sound);
+}
+
+function rewind() {
+    currentDescription = currentDescription - 1;
+    playDescription();
+}
+
+function forward() {
+    currentDescription = currentDescription + 1;
+    playDescription();
 }
